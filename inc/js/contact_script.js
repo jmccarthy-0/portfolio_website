d2adcb694043401d63c7374e1a82f0f5c37f4700
jmccarthy-0 <69696045+jmccarthy-0@users.noreply.github.com>
2021-03-27 15:45:34 -0400

    Break up JS into indivd. page files

---
+++ inc/js/contact_script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const address = document.getElementById('address').textContent;
+const clipBtn = document.getElementById('clipBtn');
+
+clipBtn.addEventListener('click', () => {
+     navigator.clipboard.writeText(address).then(()=>{
+         showFeedback('clipSuccess');
+     }, ()=> {
+         showFeedback('clipFail');
+     });
+});
+
+function showFeedback(feedbackId) {
+    let feedback = document.getElementById(feedbackId);
+    feedback.style.zIndex = 2;
+    feedback.style.opacity = 1;
+
+    let timeout = new Promise(resolve => {
+        setTimeout(() => {
+            feedback.style.opacity = 0;
+            resolve();
+        }, 2500);
+    });
+    timeout.then(() => {
+        setTimeout(()=> feedback.style.zIndex = 0, 250);
+    });
+}
